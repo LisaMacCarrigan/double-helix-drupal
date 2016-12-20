676336f1c5772aec879310b4a4e8e34b5b00b3c8
Lisa.MacCarrigan <Lisa.MacCarrigan@gmail.com>
2016-12-20 08:58:49 -0500

    add insha theme, add db backup

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -244,7 +244,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'dna',
+      'username' => 'dna',
+      'password' => 'dna',
+      'host' => '127.0.0.1',
+      'port' => '8889',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -276,7 +290,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = '31oP4YkpYCR6huRXyBATJ2kp-yCQLxExjrVLtwc7aeg';
 
 /**
  * Base URL (optional).
